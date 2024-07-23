8d8191f80d012ca0b6964db6333a13c572a7077a
mohamed <mohammad362622@gmail.com>
2024-07-24 01:09:16 +0300

    task5

--- Task 5.1 Loops/Task 4.py
+++
@@ -1,7 +0,0 @@
-l=[1,2,3,4,5]
-ans=True
-for i in range (len(l)-1):
-    if l[i] >l[i+1]:
-        ans = False
-print(ans)
-    
--- Task 5.1 Loops/Task 7 (Bonus).py
+++
@@ -1,9 +0,0 @@
-def decimal_to_binary(decimal):
-    binary = format(int(decimal),"b")
-    return binary
-print("decimal:",(11))
-print(decimal_to_binary(10))
-
-
-   
-
