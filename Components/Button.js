eacf1be36df7bbbcbe145ec884a06e63056c6bfb
mohamed8998 <mohammad36262@gmail.com.com>
2024-09-14 09:35:39 +0300

    any

---
+++ Components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button (props) {
+    let text= props.text;
+    return <button>{text}</button>;
+}
+
+
+export default Button
---
+++ Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header (props){
+    let text = props.text
+    return <h1>{text}</h1>
+}
+
+
+export default Header
