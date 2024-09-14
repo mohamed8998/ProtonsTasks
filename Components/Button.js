cb180a071fc029688cc9dadf4d62ddf616174aae
mohamed8998 <mohammad36262@gmail.com.com>
2024-09-14 09:38:59 +0300

    last commit

--- Components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Button (props) {
-    let text= props.text;
-    return <button>{text}</button>;
-}
-
-
-export default Button
--- Components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Header (props){
-    let text = props.text
-    return <h1>{text}</h1>
-}
-
-
-export default Header
